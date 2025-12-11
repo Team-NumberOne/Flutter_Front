00161bcf518dc364b0104bc49bf2335c9fc3deb6
seunghee <victoria9981@naver.com>
2025-12-11 22:28:46 +0900

    feat: 빌드 오류 해결

--- android/app/src/main/kotlin/com/numberone/daepiro/MainActivity.kt
+++ android/app/src/main/kotlin/com/numberone/daepiro/MainActivity.kt
@@ -1,4 +1,4 @@
 package com.numberone.daepiro
-import io.flutter.embedding.android.FlutterFragmentActivity
+import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterFragmentActivity()
+class MainActivity: FlutterActivity()
